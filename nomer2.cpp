f6109cd0eb70ee15fcb288917cb2278168a38bc7
Vladislava <122866844+SleEpyHeDgEhOg6@users.noreply.github.com>
2024-10-05 14:54:27 +0300

    Update nomer2.cpp

--- nomer2.cpp
+++ nomer2.cpp
@@ -1,14 +1,18 @@
 int main(){ //На поле positionA(a, b) расположена ладья. Записать условие, при котором она угрожает полю positionB(c, d).
     int A;
     int B;
-    using byte = unsigned char;
-    std::pair<byte, byte> position;
-    std::cin >> position.first >> position.second;
-    std::pair<byte, byte> position1;
-    std::cin >> position1.first >> position1.second;
-    if((1<=position.first<=8) && (1<=position.second<=8) && (1<=position1.first<=8) && (1<=position1.second<=8)){
-        if(position.first==position1.first || position.second==position1.second){
-            std::cout<<"Ладья угрожает B"<<std::endl;
+    int a,b,c,d;
+    cout<<"a: ";
+    cin>>a;
+    cout<<"b: ";
+    cin>>b;
+    cout<<"c: ";
+    cin>>c;
+    cout<<"d: ";
+    cin>>d;
+    if(a<=8 && a>=1 && b<=8 && b>=1 && c>=1 && c<=8 && d>=1 && d<=8){
+        if(a==c || b==d){
+          std::cout<<"Ладья угрожает B"<<std::endl;  
         }
         else{
             std::cout<<"Ладья не угрожает B"<<std::endl;
